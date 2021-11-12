a088bea5e2fdbaef5e80fcc910b361f5fbdd3383
Sharad Chand <pepsighan@sharadchand.com>
2021-11-12 17:02:56 +0545

    feat(scaffold): add a wrapper func to create selectors

--- starport/pkg/clipper/protoselection.go
+++ starport/pkg/clipper/protoselection.go
@@ -12,3 +12,24 @@ type ProtoPositionSelectorResult struct {
 
 // ProtoPositionSelector is a configurable selector which can select a position in code.
 type ProtoPositionSelector func(code string, options SelectOptions) (*ProtoPositionSelectorResult, error)
+
+// protoPositionFinder tries to find a required position during a walk of the protobuf AST.
+type protoPositionFinder func(sourcePos *ProtoPositionSelectorResult, options SelectOptions) ast.VisitFunc
+
+// wrapFinder creates a selector out of each finder.
+func wrapFinder(find protoPositionFinder) ProtoPositionSelector {
+	return func(content string, options SelectOptions) (*ProtoPositionSelectorResult, error) {
+		parsedAST, err := parseProto(content)
+		if err != nil {
+			return nil, err
+		}
+
+		if options == nil {
+			options = SelectOptions{}
+		}
+
+		result := &ProtoPositionSelectorResult{}
+		ast.Walk(parsedAST, find(result, options))
+		return result, nil
+	}
+}
