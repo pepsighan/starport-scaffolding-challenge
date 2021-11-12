ab8b6fb6e2c7f96cc4e9bc00b5ce54ab0a174b12
Sharad Chand <pepsighan@sharadchand.com>
2021-11-12 16:58:51 +0545

    refactor(scaffold): rename variable

--- starport/pkg/clipper/protoparser.go
+++ starport/pkg/clipper/protoparser.go
@@ -9,18 +9,17 @@ import (
 	"github.com/jhump/protoreflect/desc/protoparse/ast"
 )
 
-// parseProto parses the content as a protobuf file. Content is a the .proto file
-// read as string.
-func parseProto(content string) (*ast.FileNode, error) {
+// parseProto parses the code as a protobuf file.
+func parseProto(code string) (*ast.FileNode, error) {
 	parser := protoparse.Parser{
 		// protoparse library requires a file to parse, so we modify its accessor
 		// to allow a string to behave as a file.
 		Accessor: func(filename string) (io.ReadCloser, error) {
-			return ioutil.NopCloser(strings.NewReader(content)), nil
+			return ioutil.NopCloser(strings.NewReader(code)), nil
 		},
 	}
 
-	// It does not matter what filename is passed, it is only going to read the passed content.
+	// It does not matter what filename is passed, it is only going to read the passed code.
 	parsed, err := parser.ParseToAST("file.proto")
 	if err != nil {
 		return nil, err
