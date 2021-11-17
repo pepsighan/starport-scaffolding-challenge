df9dac65517294af4f2326012353c5edefe94df3
Sharad Chand <pepsighan@sharadchand.com>
2021-11-17 17:19:31 +0545

    test(scaffold): pasting new go code in the selections

--- starport/pkg/clipper/clipper_test.go
+++ starport/pkg/clipper/clipper_test.go
@@ -218,3 +218,141 @@ func main() {}
 		t.Fatal("incorrect generation: \n", generated)
 	}
 }
+
+func TestAddingConstDeclarationGlobally(t *testing.T) {
+	generated, err := PasteCodeSnippetAt(
+		"test.go",
+		noImportGoFile,
+		GoSelectNewGlobalPosition,
+		nil,
+		"\n\nconst a = 5",
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	correct := `package test
+
+const a = 5
+
+func main() {}
+`
+
+	if generated != correct {
+		t.Fatal("incorrect generation: \n", generated)
+	}
+}
+
+func TestAddingConstDeclarationGloballyAfterImports(t *testing.T) {
+	generated, err := PasteCodeSnippetAt(
+		"test.go",
+		groupImportGoFile,
+		GoSelectNewGlobalPosition,
+		nil,
+		"\n\nconst a = 5",
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	correct := `package test
+
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+)
+
+const a = 5
+
+func main() {}
+`
+
+	if generated != correct {
+		t.Fatal("incorrect generation: \n", generated)
+	}
+}
+
+func TestAddingBeforeExplicitReturnInFunction(t *testing.T) {
+	generated, err := PasteCodeSnippetAt(
+		"test.go",
+		withReturnGoFile,
+		GoSelectBeforeFunctionReturnsPosition,
+		SelectOptions{
+			"functionName": "withReturn",
+		},
+		"b := 5\n\t",
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	correct := `package rets
+
+func withReturn() int {
+	a := 5
+	b := 5
+	return a
+}
+`
+
+	if generated != correct {
+		t.Fatal("incorrect generation: \n", generated)
+	}
+}
+
+func TestAddingBeforeFunctionEnds(t *testing.T) {
+	generated, err := PasteCodeSnippetAt(
+		"test.go",
+		noReturnGoFile,
+		GoSelectBeforeFunctionReturnsPosition,
+		SelectOptions{
+			"functionName": "withNoReturn",
+		},
+		"\n\tb := 5",
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	correct := `package rets
+
+func withNoReturn() int {
+	a := 5
+	fmt.Println(a)
+	b := 5
+}
+`
+
+	if generated != correct {
+		t.Fatal("incorrect generation: \n", generated)
+	}
+}
+
+func TestAddingFirstInTheFunction(t *testing.T) {
+	generated, err := PasteCodeSnippetAt(
+		"test.go",
+		noReturnGoFile,
+		GoSelectStartOfFunctionPosition,
+		SelectOptions{
+			"functionName": "withNoReturn",
+		},
+		"\n\tb := 5",
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	correct := `package rets
+
+func withNoReturn() int {
+	b := 5
+	a := 5
+	fmt.Println(a)
+}
+`
+
+	if generated != correct {
+		t.Fatal("incorrect generation: \n", generated)
+	}
+}
