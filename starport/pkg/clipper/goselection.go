b0861962f95ddbbd69147f5f8b20ef3fd6bc1e13
Sharad Chand <pepsighan@sharadchand.com>
2021-11-17 08:57:35 +0545

    feat(scaffold): add a selector for new global position

--- starport/pkg/clipper/goselection.go
+++ starport/pkg/clipper/goselection.go
@@ -53,30 +53,52 @@ func wrapGoFinder(finder goPositionFinder) PositionSelector {
 }
 
 // GoSelectNewImportPosition selects a position where in a new import can be added.
-var GoSelectNewImportPosition = wrapGoFinder(func(result *PositionSelectorResult, options SelectOptions) goVisitor {
-	return func(node ast.Node) bool {
-		switch n := node.(type) {
-		case *ast.File:
-			// Adds new import after package declaration: `package name`.
-			result.OffsetPosition = OffsetPosition(n.Name.End())
-			result.Data = GoNewImportPositionData{
-				ShouldAddNewLine: true,
-			}
-		case *ast.GenDecl:
-			if n.Tok == token.IMPORT {
-				// Adds new import after the last import URL.
-				result.OffsetPosition = OffsetPosition(n.Specs[len(n.Specs)-1].End())
-				result.Data = GoNewImportPositionData{}
+var GoSelectNewImportPosition = wrapGoFinder(
+	func(result *PositionSelectorResult, options SelectOptions) goVisitor {
+		return func(node ast.Node) bool {
+			switch n := node.(type) {
+			case *ast.File:
+				// Adds new import after package declaration: `package name`.
+				result.OffsetPosition = OffsetPosition(n.Name.End())
+				result.Data = GoNewImportPositionData{
+					ShouldAddNewLine: true,
+				}
+			case *ast.GenDecl:
+				if n.Tok == token.IMPORT {
+					// Adds new import after the last import URL.
+					result.OffsetPosition = OffsetPosition(n.Specs[len(n.Specs)-1].End())
+					result.Data = GoNewImportPositionData{}
 
-				// If this is a group import, only URL is needed for the new one.
-				if n.Lparen != token.NoPos {
-					result.Data = GoNewImportPositionData{
-						OnlyURLNeeded: true,
+					// If this is a group import, only URL is needed for the new one.
+					if n.Lparen != token.NoPos {
+						result.Data = GoNewImportPositionData{
+							OnlyURLNeeded: true,
+						}
 					}
 				}
 			}
-		}
 
-		return true
-	}
-})
+			return true
+		}
+	},
+)
+
+// GoSelectNewGlobalPosition selects a position a new variable declaration, function or anything global can be
+// added.
+var GoSelectNewGlobalPosition = wrapGoFinder(
+	func(result *PositionSelectorResult, options SelectOptions) goVisitor {
+		return func(node ast.Node) bool {
+			// Select a position after the package declaration or all the imports.
+			switch n := node.(type) {
+			case *ast.File:
+				result.OffsetPosition = OffsetPosition(n.Name.End())
+			case *ast.GenDecl:
+				if n.Tok == token.IMPORT {
+					result.OffsetPosition = OffsetPosition(n.End())
+				}
+			}
+
+			return true
+		}
+	},
+)
